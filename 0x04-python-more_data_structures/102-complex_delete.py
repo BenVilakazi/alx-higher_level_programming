0e56cbd71aa08157413613167e81a645b84181a7
Ben Vilakazi <116912017+BenVilakazi@users.noreply.github.com>
2023-11-08 13:01:23 +0200

    Update 102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -6,4 +6,4 @@ def complex_delete(a_dictionary, value):
             keys_to_del.append(key)
     for key in keys_to_del:
         del a_dictionary[key]
-    return a_dictionary 
+    return a_dictionary
