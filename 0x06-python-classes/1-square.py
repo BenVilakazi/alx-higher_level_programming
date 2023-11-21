caf1cf89380f3cd3a3519c3b2094643d8ac9bd65
Ben Vilakazi <116912017+BenVilakazi@users.noreply.github.com>
2023-11-21 13:51:21 +0200

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -8,7 +8,7 @@ class Square:
     def __init__(self, size):
         """Initialize a new Square.
 
-        Args:
+        Args: 
             size (int): The size of the new square.
         """
         self.__size = size
