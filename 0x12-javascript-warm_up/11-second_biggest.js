183d6694a898883f2f94e10248f62dd8af096c72
Ben Vilakazi <116912017+BenVilakazi@users.noreply.github.com>
2024-02-12 14:03:38 +0200

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,33 @@
-#!/usr/bin/node 
+#!/usr/bin/node
+const args = process.argv;
+let i = 2;
+let res = 0;
+let maxIndex = 0;
+let temp = 0;
+const list = [];
+const len = args.length;
 
-if (process.argv.length <= 3) {
-    console.log(0);
-  } else {
-    const args = process.argv
-      .map(Number)
-      .slice(2, process.argv.length)
-      .sort((a, b) => a - b);
-    console.log(args[args.length - 2]);
+for (i = 2; i < len; i++) {
+  list.push(parseInt(args[i]));
+}
+for (i = 2; i < len; i++) {
+  if (parseInt(args[i]) > parseInt(temp)) {
+    temp = parseInt(args[i]);
+    maxIndex = i;
   }
+}
+
+temp = 0;
+i = 2;
+
+if (!args[2] || !args[3]) console.log(0);
+else {
+  while (i < len) {
+    if (args[i] < args[maxIndex] && args[i] > temp && i !== maxIndex) {
+      temp = args[i];
+      res = args[i];
+    }
+    i++;
+  }
+  console.log(res);
+}
