ac218f27f26a093505d3879386e5b0dce541f514
Ben Vilakazi <xolanithengubane@gmail.com>
2024-02-16 08:42:41 +0200

    mmm

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,24 +1,34 @@
 #!/usr/bin/python3 
 
-"""script to list all state objects using sqlalchemy
 """
-from model_state import Base, State
+This script deletes all State objects
+with a name containing the letter `a`
+from the database `hbtn_0e_6_usa`.
+"""
 
+from sys import argv
+from model_state import State, Base
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from sqlalchemy import (create_engine)
+if __name__ == "__main__":
+    """
+    Deletes State objects on the database.
+    """
 
-import sys
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
 
-
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    # create custom session object class from database engine
+    engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)
-    # create instance of new custom session class
+
     session = Session()
+
     states = session.query(State).filter(State.name.contains('a'))
-    for state in states:
-        session.delete(state)
+    if states is not None:
+        for state in states:
+            session.delete(state)
+
     session.commit()
+
+    session.close()
