5d5b429305cc16dd9b1e610d69dd078ec7abd3bf
Ben Vilakazi <116912017+BenVilakazi@users.noreply.github.com>
2024-03-02 11:14:37 +0200

    Update 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python3 
 """Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
 Usage: ./8-json_api.py <letter>
   - The letter is sent as the value of the variable `q`.
